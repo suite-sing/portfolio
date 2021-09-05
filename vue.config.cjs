da07c265bcb49cdfbc6603bec464c264e6adefe6
suite-sing <89433079+suite-sing@users.noreply.github.com>
2021-09-06 01:01:42 +0900

    buildの設定を再変更

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,11 +1,4 @@
 module.exports = {
   "publicPath": "/",
-  "outputDir": "docs",
-  configureWebpack: {
-    resolve: {
-      alias: {
-        vue$: 'vue/dist/vue.esm.js',
-      },
-    },
-  },
+  "outputDir": "docs"
 }
