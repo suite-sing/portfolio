a4df9e9473a01fcbc2d9e6661a3f780788d4cdab
suite-sing <89433079+suite-sing@users.noreply.github.com>
2021-09-05 20:32:01 +0900

    内容とDocを追加

---
+++ src/components/Equipment.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Equipment</h1>
+    <ul>
+      <li>Adobe Audition 2021</li>
+      <li>vocal shifter(有償版)</li>
+      <li>iZotope Nectar 3 Elements</li>
+      <li>Waves Audio OVox Vocal Resynth</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.icon {
+  width: 300px;
+  height: 300px;
+}
+ul {
+  display: inline-block;
+}
+li {
+  text-align: left;
+  margin: -300 -30px;
+}
+</style>
